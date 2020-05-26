34502e4e6fa5df8206133f011df55bb664681ea3
Noah Castetter <noahcastetter@gmail.com>
2020-05-25 20:59:41 -0500

    Trying to fix newline problem when pasting

--- RMHPD.sh
+++ RMHPD.sh
@@ -24,12 +24,16 @@
 # =========================================================================
 declare -r TB_DIR_ID=$(date +%S%N)
 declare -r SOURCES_DEST='./docsfrontpage/source/'
-declare -r OUTPUT_DIR=",/${TB_DIR_ID}_RMHPDF_OUT"
+declare -r OUTPUT_DIR="./${TB_DIR_ID}_RMHPDF_OUT"
 declare -r TMP_DAT_PATH="./${OUTPUT_DIR}/${TB_DIR_ID}.dat"
+
+# Delimitor for space seperated list of files who
+# satisfy file extension requirements
+OIFS=$IFS
+IFS=" "
 
 # If no files are put into this dir, then it is deleted at the end script.
 # Puting a temp file to print file paths to
-# TODO: Make sure you actually do that tho.
 mkdir "${OUTPUT_DIR}"
 touch "${TMP_DAT_PATH}"
 # =========================================================================
@@ -38,7 +42,6 @@ touch "${TMP_DAT_PATH}"
 
 # Print the name of each file found to standard out in parallel and 
 # copy them into the ./docsfrontpage/source directory.
-# TODO: MAKE SURE TO DELETE TMP_DAT FILE
 # =========================================================================
 find "$1" -type f -name "*.aadoc" >> "$TMP_DAT_PATH"
 find "$1" -type f -name "*.wiki" >> "$TMP_DAT_PATH"
@@ -47,11 +50,16 @@ find "$1" -type f -name "*.adoc" >> "$TMP_DAT_PATH"
 # Taking every line of output and putting into an array called targets
 targets=$( ./ReadFileLines.exe "$TMP_DAT_PATH" )
 
+echo "check sources dest = ${SOURCES_DEST}" 
 # Looping through targets and making a copy of every file in the 
 # targets array in the $SOURCES_DEST
 for i in ${targets[@]}; do
     cp ${targets[$i]} ${SOURCES_DEST}
 done
 
+# Garbage Collection
+# =========================================================================
+unset IFS
+rm "$TMP_DAT_PATH"
 # =========================================================================
 
