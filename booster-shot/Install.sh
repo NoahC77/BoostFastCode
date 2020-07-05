412e9ef0b9cdda37ef55092f75417ebe7bb25618
root <root@LAPTOP-SEK0JGK8.localdomain>
2020-07-05 17:12:53 -0500

    Safety Commit and Push: Add Windows 10 WSL 2 Ubuntu 20.03 portability

--- booster-shot/Install.sh
+++ booster-shot/Install.sh
@@ -2,6 +2,8 @@
 
 
 # Change $SHELL path to pwd/BoostFastCode/booster-shot/.bashrc and store old $SHELL contents into /pwd/BoostFastCode/booster-shoot/careful-for-your-health.conf
+
+TARGET_BASH_PROFILE='~/.bash_profile'
 # TODO: look into minw configurations
 BFC_ROOT_DIR="$(pwd)"
 META_DATA_FILE="/meta.dat"
@@ -9,9 +11,10 @@ META_DATA_FILE="/meta.dat"
 # Keeping a record of the .bashrc being replaced
 PREVIOUS_SHELL_RECORD="$(date +%s)"
 
-cp "$SHELL/.bashrc" "$BFC_ROOT_DIR/shell-history/$(date +%s).bashrc"
+sudo cp "$TARGET_BASH_PROFILE" "$BFC_ROOT_DIR/shell-history/$(date +%s)$TARGET_BASH_PROFILE"
 
-echo "Going to copy $SHELL/.bashrc into $BFC_ROOT_DIR/shell-history/$(date +%s).bashrc"
+sudo echo "Going to copy $TARGET_BASH_PROFILE into $BFC_ROOT_DIR/shell-history/$(date +%s)$TARGET_BASH_PROFILE"S
 
-# Yeah I did it
-cp "$BFC_ROOT_DIR/.bashrc" "$SHELL/.bashrc"
+# copt boost shot .bashrc into the system specified .bashrc path
+sudo echo "Going to copy $BFC_ROOT_DIR/.bash_profile into $TARGET_BASH_PROFILE "
+sudo cp "$BFC_ROOT_DIR/.bash_profile" "$TARGET_BASH_PROFILE"
