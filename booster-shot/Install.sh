3ebca9b8ea9f6403da142c9d88c0773daca120d4
NoahC77 <Golden21Silver12>
2020-07-08 14:58:00 -0500

    Safety Commit

--- booster-shot/Install.sh
+++ booster-shot/Install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+# =======================================================================
+#
+# File: Install.sh
+#
+# Author: Noah Castetter nbc170001@utdallas.edu
+# Version: 1
+# Created: 7-8-2020
+# Updated: 7-8-2020
+#
+# Description: Install.sh is the shell script which allows all of BoostFastCode's boosts to be used from any directory. This is done by saving a record of the original bash.bashrc file in booster-shot/shell-history. Each previous bash.bashrc is recorded with in the following format as their file name:
+# <Unix-Epoch>.bash_profile
+#
+# WARNING: WHILE WE ARE RECORDING PREVIOUS bash.bashrc FILES WE ARE STILL AUTOMATING CHANGING bash.bashrc FILES SO PROCEED WITH CAUTION
+#
+# USAGE: ./Install.sh
+# =======================================================================
 
-
-# Change $SHELL path to pwd/BoostFastCode/booster-shot/.bashrc and store old $SHELL contents into /pwd/BoostFastCode/booster-shoot/careful-for-your-health.conf
+# Location of bashrc is /etc/bash.bashrc - as of 7-8-2020
+# TARGET_BASH_RC is the location of the system's active bash.bashrc
+TARGET_BASH_RC='/etc/bash.bashrc'
 
 # TODO: look into minw configurations
-META_DATA_FILE="/meta.dat"
+META_DATA_FILE='/meta.dat'
 
 # Keeping a record of the .bashrc being replaced
 PREVIOUS_SHELL_RECORD="$(date +%s)"
 BFC_ROOT_DIR="$(pwd)"
-TARGET_BASH_RC='/etc/bash.bashrc'
 sudo cp "$TARGET_BASH_RC" "$BFC_ROOT_DIR/shell-history/$(date +%s).bash_profile"
 
 NEW_BASH_PROFILE="$BFC_ROOT_DIR/bash.bashrc"
