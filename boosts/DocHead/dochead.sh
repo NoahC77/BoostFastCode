495819d9b31e50776de4997b8fe4c604cecc9c18
NoahC77 <noahcastetter@gmail.com>
2020-07-14 16:01:10 -0500

    Patch empty target file dochead errors: see boosts/DocHead/DocheadPatchBranch/ErrorInventory.adoc

--- boosts/DocHead/dochead.sh
+++ boosts/DocHead/dochead.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # =========================================================================
 #
 # File: dochead.sh
@@ -8,7 +7,7 @@
 # Author: Noah Castetter
 # Version: 1
 # Created: 5-22-2020
-# Updated: 7-8-2020
+# Updated: 7-14-2020
 #
 # Description: A shell script which takes the name of a file and depending
 # on the extension of the file pastes a header format to documentation or
@@ -25,12 +24,18 @@ declare -r ERR_MISSING_SOURCE="ERROR: MISSING SOURCE FILE PATH CMD ARG. Try doch
 declare -r ERR_INVALID_EXTENSION="ERROR: INVALID FILE EXTENSION ON THE FIRST ARGUMENT PASSED"
 declare -r TMP_TARGET_FILE_PATH="./tmp.txt"
 declare -r LINE_FILE_PATH="$DOCHEAD_ROOT_DIR/lineFile.txt"
+declare -r TEX_RESUME_HEADER_FILE_PATH="$DOCHEAD_ROOT_DIR/docheaders/tex-resume-template.txt"
 declare -r SHELL_HEADER_FILE_PATH="$DOCHEAD_ROOT_DIR/docheaders/shellheader.txt"
 declare -r CPP_HEADER_FILE_PATH="$DOCHEAD_ROOT_DIR/docheaders/cppheader.txt"
 declare -r ADOC_DOC_HEADER="$DOCHEAD_ROOT_DIR/docheaders/adoc-doc-template.txt"
 declare -r ADOC_TABLE_HEADER="$DOCHEAD_ROOT_DIR/docheaders/adoc-table-template.txt"
 
+# Giving all permissions to target file specified by
+# first command line arg.
 sudo chmod 777 $1
+
+# Creating temp file to hold original file contents
+sudo touch $TMP_TARGET_FILE_PATH
 sudo chmod 777 $TMP_TARGET_FILE_PATH
 
 # If the source arg isn't null, then we will see if it's extension is 
@@ -45,6 +50,12 @@ else
     filename=$(basename -- "$1")
     extension="${filename##*.}"
 # =========================================================================
+   
+   # Garbage Collection
+   # Used at the end of each case to delete the temporary file
+   garb_coll () {
+      rm $TMP_TARGET_FILE_PATH
+   } 
 
     # Copying contents of file path specified in first command line argument into temp file.
     cp $1 $TMP_TARGET_FILE_PATH
@@ -64,6 +75,7 @@ else
 		      cat $ADOC_DOC_HEADER > $1
     		      cat $LINE_FILE_PATH >> $1
 		      cat $TMP_TARGET_FILE_PATH >> $1
+		      garb_coll
  		      exit
 	   	      ;;
                 
@@ -72,6 +84,7 @@ else
   		      cat $ADOC_TABLE_HEADER > $1
 		      cat $LINE_FILE_PATH >> $1
 		      cat $TMP_TARGET_FILE_PATH >> $1
+ 		      garb_coll
 		      exit
 		      ;;
     		  esac
@@ -79,31 +92,35 @@ else
     ;;
 
     tex)
-	cat $TEX_RESUME_TEMPLATE_FILE_PATH > $1
+	cat $TEX_RESUME_HEADER_FILE_PATH > $1
 	cat $LINE_FILE_PATH >> $1
 	cat $TMP_TARGET_FILE_PATH >> $1
+        garb_coll
+	exit
     ;;
 
     cpp)
 	cat $CPP_HEADER_FILE_PATH > $1
 	cat $LINE_FILE_PATH >> $1
 	cat $TMP_TARGET_FILE_PATH >> $1
+        garb_coll
+        exit
     ;;
 
     sh)
 	cat $SHELL_HEADER_FILE_PATH > $1
 	cat $LINE_FILE_PATH >> $1
 	cat $TMP_TARGET_FILE_PATH >> $1
+        garb_coll
+        exit
     ;;
 
     *)
         echo "$ERR_INVALID_EXTENSION"
+        garb_coll
     ;;
     esac
 
 fi
-
-# Garbage Collection
-rm $TMP_TARGET_FILE_PATH
 
 exit
