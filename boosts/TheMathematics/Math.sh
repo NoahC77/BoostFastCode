0cbda6389ae736072aa17e27494c5a92c7804e53
NoahC77 <noahcastetter@gmail.com>
2020-07-23 16:02:54 -0500

    Add proper table of contents stuff in headers.

---
+++ boosts/TheMathematics/Math.sh
@@ -0,0 +1,25 @@
+# =========================================================================
+#
+# File: Math.sh
+#
+# Author: Noah Castetter
+# Version: 0
+# Created:7-22-2020
+# Updated: 7-22-2020
+#
+# Description: A shell script which when called displays user defined
+# personal information when Math.sh is called. 
+#
+# WARNING:
+#
+# USAGE:
+# =========================================================================
+
+
+
+#!/bin/bash
+
+
+while IFS=, read -ra arr; do
+
+done < file
